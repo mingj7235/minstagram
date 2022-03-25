cb8da77402309bb055f16bf1d5d1834545aa2229
김민재 <joshuara7235@gmail.com>
2022-03-25 21:56:52 +0900

    git first commit for dependencies

---
+++ src/test/java/com/joshua/minstagram/MinstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshua.minstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinstagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
