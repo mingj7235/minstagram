e7aca35785f6a1291c10ed4ddaf17cde55fa1550
김민재 <joshuara7235@gmail.com>
2022-03-29 21:03:05 +0900

    의존성 변경 - MariaDB -> Mysql

--- src/main/java/com/joshua/minstagram/domain/user/entity/User.java
+++ src/main/java/com/joshua/minstagram/domain/user/entity/User.java
@@ -2,6 +2,8 @@ package com.joshua.minstagram.domain.user.entity;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.joshua.minstagram.domain.images.entity.Image;
 import com.joshua.minstagram.global.base.BaseTime;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -10,6 +12,9 @@ import org.hibernate.annotations.DynamicUpdate;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter // TODO : Setter private 으로 static 메소드 변경
@@ -43,8 +48,8 @@ public class User extends BaseTime {
 
     private String profileImage; // profile photo path + photo name
 
-//    @OneToMany(mappedBy = "user")
-//    @JsonIgnoreProperties({"user", "tags", "likes"})
-//    private List<Image> images = new ArrayList<>();
-//
+    @OneToMany(mappedBy = "user")
+    @JsonIgnoreProperties({"user", "tags", "likes"})
+    private List<Image> images = new ArrayList<>();
+
 }
